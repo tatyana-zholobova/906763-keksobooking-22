05a3230a8026c29c23d43a5ef9b4c7e9d5dfa23f
tatyana-zholobova <44976824+tatyana-zholobova@users.noreply.github.com>
2021-03-04 18:18:56 +0300

    Изменение значения минимальной цены при выборе типа размещения

--- js/form.js
+++ js/form.js
@@ -2,8 +2,17 @@ const adForm = document.querySelector('.ad-form');
 const adFormFielsets = adForm.querySelectorAll('.ad-form-header, .ad-form__element');
 const filterForm = document.querySelector('.map__filters');
 const filterFormFields = filterForm.querySelectorAll('.map__filter, .map__features');
-const addressInrut = adForm.querySelector('#address')
-
+const addressInrut = adForm.querySelector('#address');
+const priceInput = adForm.querySelector('#price');
+const typeOfPlacement = adForm.querySelector('#type');
+const timeIn = adForm.querySelector('#timein');
+const timeOut = adForm.querySelector('#timeout');
+const minPrice = {
+  bungalow: '0',
+  flat: '1000',
+  house: '5000',
+  palace: '10000',
+}
 
 const makeDisabled = (element) => {
   element.setAttribute('disabled', '')
@@ -25,5 +34,18 @@ const activateForm = () => {
   filterForm.classList.remove('map__filters--disabled');
   filterFormFields.forEach(makeActive);
 }
+
+typeOfPlacement.addEventListener('change', () => {
+  priceInput.placeholder = minPrice[typeOfPlacement.value];
+  priceInput.min = minPrice[typeOfPlacement.value];
+})
+
+timeIn.addEventListener('change', () => {
+  timeOut.value = timeIn.value;
+})
+
+timeOut.addEventListener('change', () => {
+  timeIn.value = timeOut.value;
+})
 
 export { activateForm, addressInrut }
